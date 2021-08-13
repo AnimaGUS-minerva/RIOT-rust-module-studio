d33d9bae8e16f3ede127e071708955e11a3a688b
j-devel <j@w3reality.com>
2021-08-13 15:14:02 +0700

    Build riot.esp32.bin that's linked with rustmod.a

---
+++ examples/esp32-no_std/include/rustmod.h
@@ -0,0 +1,2 @@
+
+int double_input(int input);
--- examples/esp32-no_std/riot/main.c
+++ examples/esp32-no_std/riot/main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-//#include "foo.h"
+#include "rustmod.h"
 
 int main(void)
 {
     printf("riot: hello\n");
-/*
+
     int input = 21;
+
+    printf("riot: before calling rustmod\n");
     int output = double_input(input);
+    printf("riot: after calling rustmod\n");
+
     printf("riot: %d * 2 = %d\n", input, output);
-*/
+
     printf("riot: done\n");
 
     return 0;
