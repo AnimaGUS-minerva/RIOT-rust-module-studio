5d19f7ab815d5bff628e262b2d6e85338c0c054b
j-devel <j@w3reality.com>
2021-08-13 13:05:30 +0700

    Fix format

--- examples/xtensa.mk
+++ examples/xtensa.mk
@@ -6,15 +6,15 @@ TOOLCHAIN_XTENSA := $(RUST_MODULE_STUDIO)/toolchain/xtensa
 build-module:
 	@#==== begin: compiler-builtins workaround
 	@if [ ! -d "$(TOOLCHAIN_XTENSA)/xtensa-esp32-none-elf" ]; then \
-		echo "Setting up pre-built xtensa-esp32-none-elf to workaround build errors of the compiler_builtins crate"; \
-		(cd $(TOOLCHAIN_XTENSA); curl -O -L https://github.com/AnimaGUS-minerva/RIOT-rust-module-studio/releases/download/assets-0.1/xtensa-esp32-none-elf--with-compiler-builtins-0.1.32.tbz2; tar xfj xtensa-esp32-none-elf--with-compiler-builtins-0.1.32.tbz2) \
-	fi
+        echo "Setting up pre-built xtensa-esp32-none-elf to workaround build errors of the compiler_builtins crate"; \
+        (cd $(TOOLCHAIN_XTENSA); curl -O -L https://github.com/AnimaGUS-minerva/RIOT-rust-module-studio/releases/download/assets-0.1/xtensa-esp32-none-elf--with-compiler-builtins-0.1.32.tbz2; tar xfj xtensa-esp32-none-elf--with-compiler-builtins-0.1.32.tbz2) \
+        fi
 	@mkdir -p target/sysroot/lib/rustlib && \
-		cd target/sysroot/lib/rustlib && \
-		ln -sf $(TOOLCHAIN_XTENSA)/xtensa-esp32-none-elf .
+        cd target/sysroot/lib/rustlib && \
+        ln -sf $(TOOLCHAIN_XTENSA)/xtensa-esp32-none-elf .
 	@#==== end: compiler-builtins workaround
 	RUST_MODULE_STUDIO=$(RUST_MODULE_STUDIO) source ../xtensa.setup && \
-		cargo +xtensa xbuild --release --target xtensa-esp32-none-elf
+        cargo +xtensa xbuild --release --target xtensa-esp32-none-elf
 	ls -lrt target/xtensa-esp32-none-elf/release/*.a
 
 RIOT_PATH := $(TOOLCHAIN_XTENSA)/riot
