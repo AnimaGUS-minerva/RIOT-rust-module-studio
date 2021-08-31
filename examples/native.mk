9231fd8c2a2c4136e328f2ac89af076feaee7c03
j-devel <j@w3reality.com>
2021-08-31 16:21:00 +0700

    Clean up `RIOT_ELF` stuff

--- examples/native.mk
+++ examples/native.mk
@@ -5,5 +5,7 @@ native-build-module:
 	cargo +stable-i686-unknown-linux-gnu build --lib --release --target i686-unknown-linux-gnu
 	ls -lrt target/i686-unknown-linux-gnu/release/*.a
 
+RIOT_ELF := ./riot/bin/native/riot.elf
 native-build-riot:
 	cd ./riot && BOARD=native RIOTBASE=$(RUST_MODULE_STUDIO)/RIOT make
+	ldd $(RIOT_ELF) && file $(RIOT_ELF)
