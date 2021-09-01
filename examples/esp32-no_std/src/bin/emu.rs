1c9099a67c7a969e9c09667267118c931fc0aee8
j-devel <j@w3reality.com>
2021-09-01 11:18:43 +0700

    esp32-no_std: Refactor with new runner

--- examples/esp32-no_std/src/bin/emu.rs
+++
@@ -1,8 +0,0 @@
-fn main() -> std::io::Result<()> {
-    println!("src/bin/emu.rs::main(): ^^");
-
-    mcu_emu::run_qemu_xtensa("riot.esp32.bin", Some(4_000))?;
-    // mcu_emu::run_qemu_xtensa("riot.esp32.bin", None)?;
-
-    Ok(())
-}
