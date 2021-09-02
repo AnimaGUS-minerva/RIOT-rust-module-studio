0827dfdf43cfff8e869e24622a6190f24729bc27
j-devel <j@w3reality.com>
2021-09-02 16:11:56 +0700

    Clean up tests

--- examples/xbd-micropython/riot/boot.py
+++ examples/xbd-micropython/riot/boot.py
@@ -11,17 +11,22 @@ def test_eq(title, left, right):
 
 if 1:
     tpl = voucher.test_ffi()
-    test_eq('voucher.test_ffi', tpl, (42, False, None, True, False, b'\xa0\xb1\xc2\xd3\xe4\xf5', False))
+    test_eq('voucher.test_ffi',
+        tpl, (42, False, None, True, False, b'\xa0\xb1\xc2\xd3\xe4\xf5', False))
     # print(tpl)
 
-    bs = voucher.get_voucher_jada()
-    test_eq('voucher.get_voucher_jada', len(bs), 328)
-    # print(len(bs), bs, list(bs))
+    bs_jada = voucher.get_voucher_jada()
+    test_eq('voucher.get_voucher_jada', len(bs_jada), 328)
+    # print(len(bs_jada), bs_jada, list(bs_jada))
 
-    bs = voucher.get_voucher_F2_00_02()
-    test_eq('voucher.get_voucher_F2_00_02', len(bs), 771)
-    # print(len(bs), bs)
+    bs_f2 = voucher.get_voucher_F2_00_02()
+    test_eq('voucher.get_voucher_F2_00_02', len(bs_f2), 771)
+    # print(len(bs_f2), bs_f2)
 
-    bs = voucher.get_masa_pem_F2_00_02()
-    test_eq('voucher.get_masa_pem_F2_00_02', len(bs), 684)
-    # print(len(bs), bs)
+    bs_pem_f2 = voucher.get_masa_pem_F2_00_02()
+    test_eq('voucher.get_masa_pem_F2_00_02', len(bs_pem_f2), 684)
+    # print(len(bs_pem_f2), bs_pem_f2)
+
+    # TODO
+    # test_eq('voucher.validate - jada', voucher.validate(bs_jada), True)
+    # test_eq('voucher.validate - f2', voucher.validate(bs_f2, bs_pem_f2), True)
