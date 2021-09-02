2d8f0bd90162b00d93e06efa210393624586385b
j-devel <j@w3reality.com>
2021-09-02 12:05:56 +0700

    Add `vch_get_*()` interface for loading vouchers

--- examples/xbd-micropython/include/voucher.h
+++ examples/xbd-micropython/include/voucher.h
@@ -1,3 +1,5 @@
-int square(int);
-void init_logger(void);
-void test_logger(void);
+size_t vch_get_voucher_jada(uint8_t **prt);
+size_t vch_get_voucher_F2_00_02(uint8_t **prt);
+size_t vch_get_masa_pem_F2_00_02(uint8_t **prt);
+
+int vch_square(int);
--- examples/xbd-micropython/src/lib.rs
+++ examples/xbd-micropython/src/lib.rs
@@ -1,18 +1,42 @@
 #![no_std]
 
+static VOUCHER_JADA: &[u8] = core::include_bytes!(
+    concat!(env!("CARGO_MANIFEST_DIR"), "/files/voucher_jada123456789.vch"));
+static VOUCHER_F2_00_02: &[u8] = core::include_bytes!(
+    concat!(env!("CARGO_MANIFEST_DIR"), "/files/voucher_00-D0-E5-F2-00-02.vch"));
+static MASA_PEM_F2_00_02: &[u8] = core::include_bytes!(
+    concat!(env!("CARGO_MANIFEST_DIR"), "/files/masa_00-D0-E5-F2-00-02.crt"));
+
 #[no_mangle]
-pub extern fn square(input: i32) -> i32 {
+pub extern fn vch_get_voucher_jada(ptr: *mut *const u8) -> usize {
+    set_bytes(VOUCHER_JADA, ptr)
+}
+
+#[no_mangle]
+pub extern fn vch_get_voucher_F2_00_02(ptr: *mut *const u8) -> usize {
+    set_bytes(VOUCHER_F2_00_02, ptr)
+}
+
+#[no_mangle]
+pub extern fn vch_get_masa_pem_F2_00_02(ptr: *mut *const u8) -> usize {
+    set_bytes(MASA_PEM_F2_00_02, ptr)
+}
+
+fn set_bytes(bytes: &[u8], ptr: *mut *const u8) -> usize {
+    unsafe { *ptr = bytes.as_ptr(); }
+
+    bytes.len()
+}
+
+//
+
+
+
+//
+
+#[no_mangle]
+pub extern fn vch_square(input: i32) -> i32 {
     input * input
-}
-
-#[no_mangle]
-pub extern fn init_logger() {
-    //panic!(); // TODO
-}
-
-#[no_mangle]
-pub extern fn test_logger() {
-    // TODO
 }
 
 extern "C" {
@@ -23,4 +47,3 @@ extern "C" {
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     unsafe { abort(); }
 }
-
