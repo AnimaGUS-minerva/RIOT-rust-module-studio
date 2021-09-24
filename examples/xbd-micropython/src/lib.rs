5f6f8743464c8cdb108c764a8d9ff87e642f0301
j-devel <j@w3reality.com>
2021-09-24 13:20:40 +0700

    xbd-micropython: Refactor with mcu-if v0.2; remove std debug stuff

--- examples/xbd-micropython/src/lib.rs
+++ examples/xbd-micropython/src/lib.rs
@@ -1,4 +1,11 @@
 #![no_std]
+#![feature(alloc_error_handler)]
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! { mcu_if::panic(info) }
+
+#[alloc_error_handler]
+fn alloc_error(layout: mcu_if::alloc::alloc::Layout) -> ! { mcu_if::alloc_error(layout) }
 
 //
 
