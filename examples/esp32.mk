f11ec82f6dfbbcde5f4bac203038b7856ccc2873
j-devel <j@w3reality.com>
2022-01-31 16:19:52 +0700

    Update 'esp32.{setup,mk}' to use the latest rustc esp toolchain

--- examples/esp32.mk
+++ examples/esp32.mk
@@ -4,18 +4,9 @@ RUST_MODULE_STUDIO := $(shell realpath ../..)
 TOOLCHAIN_XTENSA := $(RUST_MODULE_STUDIO)/toolchain/xtensa
 
 esp32-build-module:
-	@#==== begin: compiler-builtins workaround
-	@if [ ! -d "$(TOOLCHAIN_XTENSA)/xtensa-esp32-none-elf" ]; then \
-        echo "Setting up pre-built xtensa-esp32-none-elf to workaround build errors of the compiler_builtins crate"; \
-        (cd $(TOOLCHAIN_XTENSA); curl -O -L https://github.com/AnimaGUS-minerva/RIOT-rust-module-studio/releases/download/assets-0.1/xtensa-esp32-none-elf--with-compiler-builtins-0.1.32.tbz2; tar xfj xtensa-esp32-none-elf--with-compiler-builtins-0.1.32.tbz2) \
-        fi
-	@mkdir -p target/sysroot/lib/rustlib && \
-        cd target/sysroot/lib/rustlib && \
-        ln -sf $(TOOLCHAIN_XTENSA)/xtensa-esp32-none-elf .
-	@#==== end: compiler-builtins workaround
 	RUST_MODULE_STUDIO=$(RUST_MODULE_STUDIO) source ../esp32.setup && \
         XTENSA_GCC=$(TOOLCHAIN_XTENSA)/riot/xtensa-esp32-elf/bin/xtensa-esp32-elf-gcc \
-		cargo +xtensa xbuild --lib --release --target xtensa-esp32-none-elf $(CARGO_FEATURES)
+		cargo +esp xbuild --lib --release --target xtensa-esp32-none-elf $(CARGO_FEATURES)
 	ls -lrt target/xtensa-esp32-none-elf/release/*.a
 
 RIOT_PATH := $(TOOLCHAIN_XTENSA)/riot
