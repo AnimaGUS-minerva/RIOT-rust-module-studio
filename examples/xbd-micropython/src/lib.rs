a6af284b96ec26ef1ffd1bc21b230519d5388ad5
j-devel <j@w3reality.com>
2022-02-02 15:38:06 +0700

    xbd-micropython: Build libvoucher.a against minerva_voucher 0.6.0

--- examples/xbd-micropython/src/lib.rs
+++ examples/xbd-micropython/src/lib.rs
@@ -16,9 +16,9 @@ fn alloc_error(layout: mcu_if::alloc::alloc::Layout) -> ! { mcu_if::alloc_error(
 extern crate std;
 
 #[cfg(feature = "std")]
-use std::{println, vec::Vec};
+use std::{println, vec::Vec, vec};
 #[cfg(not(feature = "std"))]
-use mcu_if::{println, alloc::vec::Vec};
+use mcu_if::{println, alloc::{vec::Vec, vec}};
 
 //
 
@@ -38,6 +38,8 @@ fn init_psa_crypto() {
 //
 
 use minerva_voucher::{Sign, Validate, SignatureAlgorithm};
+use minerva_voucher::{vrq, attr::*};
+use core::convert::TryFrom;
 
 #[cfg(not(any(feature = "x86", feature = "xtensa")))]
 use minerva_voucher::Voucher; // for x86_64{,-lts}, {x86,xtensa}-lts
@@ -45,22 +47,22 @@ use minerva_voucher::Voucher; // for x86_64{,-lts}, {x86,xtensa}-lts
 use wip::Voucher;
 
 #[cfg(any(feature = "x86", feature = "xtensa"))]
-mod wip {
+mod wip { // TODO adapt to voucher 0.6.0 -- `CARGO_FEATURES="--features xtensa"  make esp32-build-module`
     use super::*;
     use minerva_voucher::Voucher as BaseVoucher;
 
     pub struct Voucher(BaseVoucher); // dummy `Voucher` without validation capability
     impl Voucher {
-        pub fn from(raw: &[u8]) -> Self { Voucher(BaseVoucher::from(raw).unwrap()) }
+        pub fn from(raw: &[u8]) -> Self { Voucher(BaseVoucher::try_from(raw).unwrap()) }
     }
     impl core::ops::Deref for Voucher {
         type Target = BaseVoucher;
         fn deref(&self) -> &Self::Target { &self.0 }
     }
     impl Validate for Voucher {
-        fn validate(&self, _masa_pem: Option<&[u8]>) -> bool {
+        fn validate(&self, _masa_pem: Option<&[u8]>) -> Result<&Self, VoucherError> {
             println!("⚠️ WIP -- `x86`, `xtensa` (v3 with PSA); validation fails for now!!");
-            false
+            Err(VoucherError::ValidationFailed)
         }
     }
 }
@@ -70,15 +72,17 @@ mod wip {
 use mcu_if::utils::u8_slice_from;
 
 static VOUCHER_JADA: &[u8] = core::include_bytes!(
-    concat!(env!("CARGO_MANIFEST_DIR"), "/files/voucher_jada123456789.vch"));
+    concat!(env!("CARGO_MANIFEST_DIR"), "/data/jada/voucher_jada123456789.vch"));
 static VOUCHER_F2_00_02: &[u8] = core::include_bytes!(
-    concat!(env!("CARGO_MANIFEST_DIR"), "/files/voucher_00-D0-E5-F2-00-02.vch"));
+    concat!(env!("CARGO_MANIFEST_DIR"), "/data/00-D0-E5-F2-00-02/voucher_00-D0-E5-F2-00-02.vch"));
+static VOUCHER_REQUEST_F2_00_02: &[u8] = core::include_bytes!(
+    concat!(env!("CARGO_MANIFEST_DIR"), "/data/00-D0-E5-F2-00-02/vr_00-D0-E5-F2-00-02.vrq"));
 static MASA_PEM_F2_00_02: &[u8] = core::include_bytes!(
-    concat!(env!("CARGO_MANIFEST_DIR"), "/files/masa_00-D0-E5-F2-00-02.crt"));
-static KEY_PEM_02_00_2E: &[u8] = core::include_bytes!(
-    concat!(env!("CARGO_MANIFEST_DIR"), "/files/key_00-D0-E5-02-00-2E.pem"));
-static DEVICE_CRT_02_00_2E: &[u8] = core::include_bytes!(
-    concat!(env!("CARGO_MANIFEST_DIR"), "/files/device_00-D0-E5-02-00-2E.crt"));
+    concat!(env!("CARGO_MANIFEST_DIR"), "/data/00-D0-E5-F2-00-02/masa.crt"));
+static KEY_PEM_F2_00_02: &[u8] = core::include_bytes!(
+    concat!(env!("CARGO_MANIFEST_DIR"), "/data/00-D0-E5-F2-00-02/key.pem"));
+static DEVICE_CRT_F2_00_02: &[u8] = core::include_bytes!(
+    concat!(env!("CARGO_MANIFEST_DIR"), "/data/00-D0-E5-F2-00-02/device.crt"));
 
 
 #[no_mangle]
@@ -97,13 +101,13 @@ pub extern fn vch_get_masa_pem_F2_00_02(pp: *mut *const u8) -> usize {
 }
 
 #[no_mangle]
-pub extern fn vch_get_key_pem_02_00_2E(pp: *mut *const u8) -> usize {
-    set_bytes_static(KEY_PEM_02_00_2E, pp)
+pub extern fn vch_get_key_pem_F2_00_02(pp: *mut *const u8) -> usize {
+    set_bytes_static(KEY_PEM_F2_00_02, pp)
 }
 
 #[no_mangle]
-pub extern fn vch_get_device_crt_02_00_2E(pp: *mut *const u8) -> usize {
-    set_bytes_static(DEVICE_CRT_02_00_2E, pp)
+pub extern fn vch_get_device_crt_F2_00_02(pp: *mut *const u8) -> usize {
+    set_bytes_static(DEVICE_CRT_F2_00_02, pp)
 }
 
 fn set_bytes_static(bytes: &[u8], pp: *mut *const u8) -> usize {
@@ -128,7 +132,7 @@ fn set_bytes_heap(bytes: Vec<u8>, pp: *mut *const u8) -> usize {
 pub extern fn vch_debug(ptr: *const u8, sz: usize) {
     let raw_voucher = u8_slice_from(ptr, sz);
 
-    Voucher::from(raw_voucher).unwrap().dump()
+    Voucher::try_from(raw_voucher).unwrap().dump()
 }
 
 //
@@ -138,7 +142,7 @@ pub extern fn vch_validate(ptr: *const u8, sz: usize) -> bool {
     let raw_voucher = u8_slice_from(ptr, sz);
     println!("@@ validating raw_voucher: [len={}]", raw_voucher.len());
 
-    Voucher::from(raw_voucher).unwrap().validate(None)
+    Voucher::try_from(raw_voucher).unwrap().validate(None).is_ok()
 }
 
 #[no_mangle]
@@ -147,17 +151,22 @@ pub extern fn vch_validate_with_pem(ptr: *const u8, sz: usize, ptr_pem: *const u
     let pem = u8_slice_from(ptr_pem, sz_pem);
     println!("@@ validating raw_voucher with pem: [len={}] [len={}]", raw_voucher.len(), pem.len());
 
-    Voucher::from(raw_voucher).unwrap().validate(Some(pem))
+    Voucher::try_from(raw_voucher).unwrap().validate(Some(pem)).is_ok()
 }
 
 //
 
 #[no_mangle]
-pub extern fn vch_create_vrq_02_00_2E(pp: *mut *const u8) -> usize {
-    let mut vch = Voucher::new();
-    vch.set_content(&minerva_voucher::wip_vrhash_sidhash_content());
+pub extern fn vch_create_vrq_F2_00_02(pp: *mut *const u8) -> usize {
+    let mut vrq = vrq![
+        Attr::Assertion(Assertion::Proximity),
+        Attr::CreatedOn(1599086034),
+        Attr::Nonce(vec![48, 130, 1, 216, 48, 130, 1, 94, 160, 3, 2, 1, 2, 2, 1, 1, 48, 10, 6, 8, 42, 134, 72, 206, 61, 4, 3, 2, 48, 115, 49, 18, 48, 16, 6, 10, 9, 146, 38, 137, 147, 242, 44, 100, 1, 25, 22, 2, 99, 97, 49, 25, 48, 23, 6, 10, 9, 146, 38, 137, 147, 242, 44, 100, 1, 25, 22, 9, 115, 97, 110, 100, 101, 108, 109, 97, 110, 49, 66, 48, 64, 6, 3, 85, 4, 3, 12, 57, 35, 60, 83, 121, 115, 116, 101, 109, 86, 97, 114, 105, 97, 98, 108, 101, 58, 48, 120, 48, 48, 48, 48, 53, 53, 98, 56, 50, 53, 48, 99, 48, 100, 98, 56, 62, 32, 85, 110, 115, 116, 114, 117, 110, 103, 32, 70, 111, 117, 110, 116, 97, 105, 110, 32, 67, 65, 48, 30, 23, 13, 50, 48, 48, 56, 50, 57, 48, 52, 48, 48, 49, 54, 90, 23, 13, 50, 50, 48, 56, 50, 57, 48, 52, 48, 48, 49, 54, 90, 48, 70, 49, 18, 48, 16, 6, 10, 9, 146, 38, 137, 147, 242, 44, 100, 1, 25, 22, 2, 99, 97, 49, 25, 48, 23, 6, 10, 9, 146, 38, 137, 147, 242, 44, 100, 1, 25, 22, 9, 115, 97, 110, 100, 101, 108, 109, 97, 110, 49, 21, 48, 19, 6, 3, 85, 4, 3, 12, 12, 85, 110, 115, 116, 114, 117, 110, 103, 32, 74, 82, 67, 48, 89, 48, 19, 6, 7, 42, 134, 72, 206, 61, 2, 1, 6, 8, 42, 134, 72, 206, 61, 3, 1, 7, 3, 66, 0, 4, 150, 101, 80, 114, 52, 186, 159, 229, 221, 230, 95, 246, 240, 129, 111, 233, 72, 158, 129, 12, 18, 7, 59, 70, 143, 151, 100, 43, 99, 0, 141, 2, 15, 87, 201, 124, 148, 127, 132, 140, 178, 14, 97, 214, 201, 136, 141, 21, 180, 66, 31, 215, 242, 106, 183, 228, 206, 5, 248, 167, 76, 211, 139, 58, 163, 16, 48, 14, 48, 12, 6, 3, 85, 29, 19, 1, 1, 255, 4, 2, 48, 0, 48, 10, 6, 8, 42, 134, 72, 206, 61, 4, 3, 2, 3, 104, 0, 48, 101, 2, 49, 0, 135, 158, 205, 227, 138, 5, 18, 46, 182, 247, 44, 178, 27, 195, 210, 92, 190, 230, 87, 55, 112, 86, 156, 236, 35, 12, 164, 140, 57, 241, 64, 77, 114, 212, 215, 85, 5, 155, 128, 130, 2, 14, 212, 29, 79, 17, 159, 231, 2, 48, 60, 20, 216, 138, 10, 252, 64, 71, 207, 31, 135, 184, 115, 193, 106, 40, 191, 184, 60, 15, 136, 67, 77, 157, 243, 247, 168, 110, 45, 198, 189, 136, 149, 68, 47, 32, 55, 237, 204, 228, 133, 91, 17, 218, 154, 25, 228, 232]),
+        Attr::ProximityRegistrarCert(vec![102, 114, 118, 85, 105, 90, 104, 89, 56, 80, 110, 86, 108, 82, 75, 67, 73, 83, 51, 113, 77, 81]),
+        Attr::SerialNumber(b"00-D0-E5-F2-00-02".to_vec())];
+    let vrq = vrq.sign(KEY_PEM_F2_00_02, SignatureAlgorithm::ES256).unwrap();
 
-    set_bytes_heap(vch.serialize().unwrap(), pp)
+    set_bytes_heap(vrq.serialize().unwrap(), pp)
 }
 
 #[no_mangle]
@@ -169,8 +178,8 @@ pub extern fn vch_sign(
     let key = u8_slice_from(ptr_key, sz_key);
     println!("@@ vch_sign(): [len_raw={}] [len_key={}]", raw.len(), key.len());
 
-    let mut vch = Voucher::from(raw).unwrap();
-    vch.sign(key, SignatureAlgorithm::ES256);
+    let mut vch = Voucher::try_from(raw).unwrap();
+    vch.sign(key, SignatureAlgorithm::ES256).unwrap();
 
     set_bytes_heap(vch.serialize().unwrap(), pp)
 }
