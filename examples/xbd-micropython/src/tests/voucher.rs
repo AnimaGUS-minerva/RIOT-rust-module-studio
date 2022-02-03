425c4e014d5d470768a81ae6df82b60bbe0629c6
j-devel <j@w3reality.com>
2022-02-03 18:28:10 +0700

    Adapt 'src/tests/voucher.rs' to minerva_voucher 0.6.1

--- examples/xbd-micropython/src/tests/voucher.rs
+++ examples/xbd-micropython/src/tests/voucher.rs
@@ -1,14 +1,15 @@
 use crate::{VOUCHER_JADA, VOUCHER_F2_00_02, MASA_PEM_F2_00_02};
 use minerva_voucher::{Voucher, Validate, SignatureAlgorithm};
+use core::convert::TryFrom;
 
 #[test]
 fn test_voucher_decode_jada() {
     #[cfg(feature = "v3")]
     crate::init_psa_crypto();
 
-    let vch = Voucher::from(VOUCHER_JADA).unwrap();
+    let vch = Voucher::try_from(VOUCHER_JADA).unwrap();
 
-    let (sig, alg) = vch.get_signature();
+    let (sig, alg) = vch.get_signature().unwrap();
     assert_eq!(sig.len(), 64);
     assert_eq!(*alg, SignatureAlgorithm::ES256);
 
@@ -20,10 +21,10 @@ fn test_voucher_validate_jada() {
     #[cfg(feature = "v3")]
     crate::init_psa_crypto();
 
-    let vch = Voucher::from(VOUCHER_JADA).unwrap();
+    let vch = Voucher::try_from(VOUCHER_JADA).unwrap();
 
     // No external masa cert; use `signer_cert` embedded in COSE unprotected
-    assert!(vch.validate(None));
+    assert!(vch.validate(None).is_ok());
 }
 
 #[test]
@@ -31,9 +32,9 @@ fn test_voucher_decode_f2_00_02() {
     #[cfg(feature = "v3")]
     crate::init_psa_crypto();
 
-    let vch = Voucher::from(VOUCHER_F2_00_02).unwrap();
+    let vch = Voucher::try_from(VOUCHER_F2_00_02).unwrap();
 
-    let (sig, alg) = vch.get_signature();
+    let (sig, alg) = vch.get_signature().unwrap();
     assert_eq!(sig.len(), 64);
     assert_eq!(*alg, SignatureAlgorithm::ES256);
 
@@ -45,10 +46,10 @@ fn test_voucher_validate_f2_00_02() {
     #[cfg(feature = "v3")]
     crate::init_psa_crypto();
 
-    let vch = Voucher::from(VOUCHER_F2_00_02).unwrap();
+    let vch = Voucher::try_from(VOUCHER_F2_00_02).unwrap();
 
     let masa_pem = MASA_PEM_F2_00_02;
     assert_eq!(masa_pem.len(), 684);
 
-    assert!(vch.validate(Some(masa_pem)));
+    assert!(vch.validate(Some(masa_pem)).is_ok());
 }
