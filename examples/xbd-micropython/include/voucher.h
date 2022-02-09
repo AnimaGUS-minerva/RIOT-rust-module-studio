c3acbb5218d416934b3979d4526e843c77331793
j-devel <j@w3reality.com>
2022-02-09 15:29:20 +0700

    Update 'include/voucher.h'

--- examples/xbd-micropython/include/voucher.h
+++ examples/xbd-micropython/include/voucher.h
@@ -3,12 +3,13 @@ int vch_square(int);
 size_t vch_get_voucher_jada(uint8_t **pp);
 size_t vch_get_voucher_F2_00_02(uint8_t **pp);
 size_t vch_get_masa_pem_F2_00_02(uint8_t **pp);
-size_t vch_get_key_pem_02_00_2E(uint8_t **pp);
-size_t vch_get_device_crt_02_00_2E(uint8_t **pp);
+size_t vch_get_key_pem_F2_00_02(uint8_t **pp);
+size_t vch_get_device_crt_F2_00_02(uint8_t **pp);
 
-void vch_debug(const uint8_t *ptr, size_t sz);
+size_t vch_get_vrq_F2_00_02(uint8_t **pp);
+size_t vch_create_vrq_F2_00_02(uint8_t **pp);
+
+size_t vch_sign(const uint8_t *ptr_raw, size_t sz_raw, const uint8_t *ptr_key, size_t sz_key, uint8_t **pp);
 bool vch_validate(const uint8_t *ptr, size_t sz);
 bool vch_validate_with_pem(const uint8_t *ptr, size_t sz, const uint8_t *ptr_pem, size_t sz_pem);
-
-size_t vch_create_vrq_02_00_2E(uint8_t **pp);
-size_t vch_sign(const uint8_t *ptr_raw, size_t sz_raw, const uint8_t *ptr_key, size_t sz_key, uint8_t **pp);
+void vch_debug(const uint8_t *ptr, size_t sz);
