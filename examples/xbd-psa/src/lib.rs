ef6601ff16970d28deb3da9dc7ec8334dbf83216
j-devel <38098238+j-devel@users.noreply.github.com>
2022-06-30 11:50:22 +0700

    xbd-psa: WIP - cross build testing for 'psa-crypto' and 'minerva-mbedtls' crates

--- examples/xbd-psa/src/lib.rs
+++ examples/xbd-psa/src/lib.rs
@@ -10,48 +10,51 @@ fn alloc_error(layout: mcu_if::alloc::alloc::Layout) -> ! { mcu_if::alloc_error(
 //
 
 use mcu_if::println;
-use mcu_if::alloc::{boxed::Box, vec, vec::Vec};
-use mcu_if::core2::io::{self as io, Cursor, Seek, SeekFrom, Write};
+use mcu_if::alloc::{vec, vec::Vec};
 
 #[no_mangle]
 pub extern fn square(input: i32) -> i32 {
     println!("[src/lib.rs] square(): input: {}", input);
 
-    demo_alloc();
-    demo_io();
+    demo_psa();
 
     input * input
 }
 
 //
 
-fn demo_alloc() {
-    println!("Box::new(42): {:?}", Box::new(42));
-    println!("Box::new([0; 10]): {:?}", Box::new([0; 10]));
+use psa_crypto::{self, ffi};
+//==== !!!!switch
+//use minerva_mbedtls::{psa_crypto, psa_ifce};
+//use psa_crypto::ffi;
+
+fn demo_psa() {
+    // ok
+    println!("enum value of `ffi::MD_SHA256`: {:?}", ffi::MD_SHA256); // 4
+    println!("enum value of `ffi::MD_SHA384`: {:?}", ffi::MD_SHA384); // 5
+    println!("enum value of `ffi::MD_SHA512`: {:?}", ffi::MD_SHA512); // 6
+
+    //====
+    // TODO - for now, in case of arch `xtensa`, RIOT linking fails !!
+    //        https://github.com/AnimaGUS-minerva/rust-psa-crypto/commit/a9cacd4afa937ac90931c9ef054cb3119a4006ac
+    //----
+    //psa_crypto::init().unwrap(); // !!
+    //psa_crypto::initialized().unwrap(); // !!
+    //let _info = md_info::from_type(MD_SHA256); // !!
+    //====
+
+    //
+
     println!("Vec::from([0, 1, 2]): {:?}", Vec::from([0, 1, 2]));
     println!("vec![0, 1, 2]: {:?}", vec![0, 1, 2]);
 }
 
-// code adapted from https://doc.rust-lang.org/std/io/struct.Cursor.html
+//
 
-fn demo_io() {
-    let mut array = [0; 15];
-    let mut buff = Cursor::new(&mut array[..]);
-
-    write_ten_bytes_at_end(&mut buff).unwrap();
-    println!("buff: {:?}", buff);
-
-    let check = &buff.get_ref()[5..15];
-    println!("check: {:?}", check);
-    assert_eq!(check, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-}
-
-fn write_ten_bytes_at_end<W: Write + Seek>(writer: &mut W) -> io::Result<()> {
-    writer.seek(SeekFrom::End(-10))?;
-
-    for i in 0..10 {
-        writer.write(&[i])?;
+pub use ffi::{md_type_t, MD_SHA256};
+pub struct md_info(*const ffi::md_info_t);
+impl md_info {
+    pub fn from_type(ty: ffi::md_type_t) -> Self {
+        Self(unsafe { ffi::md_info_from_type(ty) })
     }
-
-    Ok(())
 }
