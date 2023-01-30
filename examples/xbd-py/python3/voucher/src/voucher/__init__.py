131c04e5e3d6aa71c14342095dbc0d8bd6106233
j-devel <38098238+j-devel@users.noreply.github.com>
2023-01-30 15:07:00 +0700

    Get `test_voucher_mbedtls_version()` to work

---
+++ examples/xbd-py/python3/voucher/src/voucher/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2023, ANIMA Minerva toolkit
+
+"""python-voucher is a wrapper to the Minerva voucher library."""
+
+from . import version as version  # 'mbed TLS 3.0.0'
+
+__version__ = "0.1.0"
+
+__all__ = (
+    "version",
+)
---
+++ examples/xbd-py/python3/voucher/test.py
@@ -0,0 +1,13 @@
+import voucher.version as mbedtls_version
+
+def test_voucher_mbedtls_version():
+    print('==== test_voucher_mbedtls_version(): ^^')
+    print(dir(mbedtls_version))
+    print('mbedtls_version.version:', mbedtls_version.version)  # mbed TLS 3.0.0
+
+def test_voucher_xx():
+    print('==== test_voucher_xx(): ^^')
+
+if 1:
+    test_voucher_mbedtls_version()
+    test_voucher_xx()
