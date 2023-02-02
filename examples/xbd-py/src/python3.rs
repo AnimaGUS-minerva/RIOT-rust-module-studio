ff6d00eb54b8fc64aa8aa38966d9db9699ea8e20
j-devel <38098238+j-devel@users.noreply.github.com>
2023-02-02 09:10:50 +0700

    Implement `voucher_version_get_string_full()`

--- examples/xbd-py/src/python3.rs
+++ examples/xbd-py/src/python3.rs
@@ -4,9 +4,8 @@ use std::io;
 #[no_mangle]
 pub extern fn voucher_version_get_string_full(ptr: *mut u8, sz: usize) {
     let mut buf = u8_slice_mut_from(ptr, sz);
-
-    let mut version: &[u8] = b"rust voucher x.x.x"; // WIP
-
     buf.fill(0u8);
-    io::copy(&mut version, &mut buf).unwrap();
+
+    io::copy(&mut format!("Rust voucher {}", minerva_voucher::VERSION).as_bytes(),
+             &mut buf).unwrap();
 }
