7a3f55114f1a7d80248814f0af3ae1899b51193f
j-devel <38098238+j-devel@users.noreply.github.com>
2023-04-13 16:39:26 +0700

    Refactor `esp32` board build scripts

--- examples/esp32.mk
+++ examples/esp32.mk
@@ -1,29 +1,33 @@
 SHELL := /bin/bash
-RUST_MODULE_STUDIO := $(shell realpath ../..)
+STUDIO := $(shell realpath ../..)
 
-TOOLCHAIN_XTENSA := $(RUST_MODULE_STUDIO)/toolchain/xtensa
+TOOLCHAIN_XTENSA := $(STUDIO)/toolchain/xtensa
+CLANG_ESP := $(HOME)/.espressif/tools/xtensa-esp32-elf-clang/esp-13.0.0-20211203-x86_64-unknown-linux-gnu
+
+export CARGO_HOME := $(STUDIO)/target/cargo
+export LIBCLANG_PATH := $(CLANG_ESP)/lib
+export XARGO_RUST_SRC := $(TOOLCHAIN_XTENSA)/rust-build/rust-src-1.57.0.2/rust-src/lib/rustlib/src/rust/library
+export PATH := $(CLANG_ESP)/bin:$(PATH)
 
 esp32-build-module:
 	@echo "Buidling 'target/xtensa-esp32-none-elf/release/*.a'"
-	RUST_MODULE_STUDIO=$(RUST_MODULE_STUDIO) source ../esp32.setup && \
-        XTENSA_GCC=$(TOOLCHAIN_XTENSA)/riot/xtensa-esp32-elf/bin/xtensa-esp32-elf-gcc \
+	XTENSA_GCC=$(CLANG_ESP)/bin/xtensa-esp32-elf-gcc \
 		cargo +esp xbuild --lib --release --target xtensa-esp32-none-elf $(CARGO_OPTS)
 
-RIOT_PATH := $(TOOLCHAIN_XTENSA)/riot
-RIOT_BASE ?= $(RUST_MODULE_STUDIO)/RIOT
+RIOT_BASE ?= $(STUDIO)/RIOT
 esp32-build-riot:
-	RUST_MODULE_STUDIO=$(RUST_MODULE_STUDIO) source ../esp32.setup && cd ./main && \
-		RIOT_PATH=${RIOT_PATH} RIOT_BASE=${RIOT_BASE} CONTINUE_ON_EXPECTED_ERRORS=1 \
-		$(TOOLCHAIN_XTENSA)/riot/riot-build
+	cd ./main && \
+		CONTINUE_ON_EXPECTED_ERRORS=1 WERROR=0 BOARD=esp32-wroom-32 RIOTBASE=$(RIOT_BASE)  make
 esp32-build-riot-micropython:
-	RUST_MODULE_STUDIO=$(RUST_MODULE_STUDIO) source ../esp32.setup && cd ./micropython/ports/riot && \
-		RIOT_PATH=${RIOT_PATH} RIOT_BASE=${RIOT_BASE} CONTINUE_ON_EXPECTED_ERRORS=1 \
-		CUSTOM_BOARD=esp32  $(TOOLCHAIN_XTENSA)/riot/riot-build
+	cd ./micropython/ports/riot && \
+		CUSTOM_BOARD=esp32 \
+		CONTINUE_ON_EXPECTED_ERRORS=1 WERROR=0 BOARD=esp32-wroom-32 RIOTBASE=$(RIOT_BASE)  make
 
 RIOT_ESP32_BIN ?= ./main.esp32.bin
-esp32-run-riot:
+esp32-build-flash:
+	python3 $(TOOLCHAIN_XTENSA)/esptool/esptool.py --chip esp32 elf2image \
+		-o $(RIOT_ESP32_BIN) main/bin/esp32-wroom-32/main.elf
+
+esp32-run-riot: esp32-build-flash
 	RIOT_ESP32_BIN=$(RIOT_ESP32_BIN) \
 		cargo run --manifest-path ../runner/Cargo.toml esp32 $(EMU_TIMEOUT)
-
-esp32-clean:
-	rm -rf $(TOOLCHAIN_XTENSA)/xtensa-esp32-none-elf
