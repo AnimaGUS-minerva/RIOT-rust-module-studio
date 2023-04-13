b2e928797897cfa7ed0e910068c23acb55394462
j-devel <38098238+j-devel@users.noreply.github.com>
2023-04-13 11:49:46 +0700

    Update elf filename

--- crates/mcu-emu/src/lib.rs
+++ crates/mcu-emu/src/lib.rs
@@ -10,9 +10,9 @@ pub fn run(args: &Vec<String>) -> std::io::Result<()> {
 
     match board {
         "native" => run_native(&std::env::var("RIOT_NATIVE_ELF")
-            .unwrap_or("./riot/bin/native/riot.elf".to_string()), timeout)?,
+            .unwrap_or("./main/bin/native/main.elf".to_string()), timeout)?,
         "esp32" => run_esp32(&std::env::var("RIOT_ESP32_BIN")
-            .unwrap_or("./riot.esp32.bin".to_string()), timeout)?,
+            .unwrap_or("./main.esp32.bin".to_string()), timeout)?,
         _ => panic!("Unsupported board: {}", board),
     }
 
