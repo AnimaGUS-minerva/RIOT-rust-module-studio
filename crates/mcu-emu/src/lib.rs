6c85111bea39b1ab5ba0ebeb784bfdb3034a3a7e
j-devel <38098238+j-devel@users.noreply.github.com>
2023-05-12 12:13:02 +0700

    mcu-emu: Refine handling `args`

--- crates/mcu-emu/src/lib.rs
+++ crates/mcu-emu/src/lib.rs
@@ -5,10 +5,11 @@ pub use native::run_native;
 pub use esp32::run_esp32;
 
 pub fn run(args: &Vec<String>) -> std::io::Result<()> {
+    let argc = args.len();
+    assert!(argc > 2);
     let board = args[1].as_str();
-    let timeout = args[2].parse::<u64>().unwrap();
-    let timeout = if timeout > 0 { Some(timeout) } else { None };
-    let nic = if args.len() > 3 { Some(args[3].as_str()) } else { None };
+    let timeout = args[2].parse::<u64>().ok().filter(|n| *n > 0);
+    let nic = if argc > 3 { Some(args[3].as_str()) } else { None };
 
     match board {
         "native" => run_native(&std::env::var("RIOT_NATIVE_ELF")
