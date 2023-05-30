2addfa911ea21f400c8f79668d57e5a03e6f8295
j-devel <38098238+j-devel@users.noreply.github.com>
2023-05-30 14:08:19 +0700

    xbd-net: WIP Refactor with "border_router.h"

--- examples/xbd-net/main/main.c
+++ examples/xbd-net/main/main.c
@@ -1,32 +1,18 @@
-/*
- * Copyright (C) 2022 HAW Hamburg
- *
- * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
- * directory for more details.
- */
 
-/**
- * @ingroup     tests
- * @{
- *
- * @file
- * @brief       Test application for ESP ethernet peripheral
- *
- * @author      Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
- *
- * @}
- */
+// TODO !!!! test esp-{wifi,now} on MCU
+// TODO refactor BR_* constants into 'minerva_esp32_gnrc/Makefile'
 
 //--------@@
 #include <stdio.h>
-#include <net/gnrc/ipv6/nib.h>
-#include <net/gnrc/ipv6.h>
-#include <net/gnrc/netapi.h>
+// TODO cleanup
+//#include <net/gnrc/ipv6/nib.h>
+//??#include <net/gnrc/ipv6.h>
+//#include <net/gnrc/netapi.h>
 #include <net/gnrc/netif.h>
-#ifdef MODULE_GNRC_RPL
-  #include <net/gnrc/rpl.h>
-#endif
+//#ifdef MODULE_GNRC_RPL
+//  #include <net/gnrc/rpl.h>
+//#endif
+
 #include <net/ethernet.h>
 #include <net/ipv6/addr.h>
 #include <net/netopt.h>
@@ -35,6 +21,10 @@
 #include <shell.h>
 #include <msg.h>
 //--------@@
+
+#if defined(MINERVA_BOARD_ESP32_ETH) || defined(MINERVA_BOARD_ESP32_WROOM32)
+  #include "border_router.h"
+#endif
 
 #ifdef MINERVA_BOARD_ESP32_ETH
 #ifdef MINERVA_DEBUG_ETH_MINIMAL
@@ -56,7 +46,6 @@ static int esp32_eth_init(void) {
 #endif
 
 //--------@@
-
 static void print_ifce(gnrc_netif_t *ifce) {
     printf("print_ifce(): ifce: %p\n", (void *)ifce);
     if (!ifce) return;
@@ -70,7 +59,6 @@ static void print_ifce(gnrc_netif_t *ifce) {
     //printf("  hint - for `native` board, try `ping6 %s%%tap1` in a new shell\n", addrstr);
     //printf("  hint - for `esp32` board, try `ping6 %s%%br0` in a new shell\n", addrstr);
 }
-
 static void find_ifces(gnrc_netif_t **outer, gnrc_netif_t **inner) {
     uint16_t mtu;
     gnrc_netif_t *netif = NULL;
@@ -97,75 +85,6 @@ static void find_ifces(gnrc_netif_t **outer, gnrc_netif_t **inner) {
     printf("@@ (esp-now) inner: %p\n", (void *)*inner);
     print_ifce(*inner);
 }
-
-#if defined(MINERVA_BOARD_ESP32_ETH) || defined(MINERVA_BOARD_ESP32_WROOM32)//!!!!cleanup
-static int set_ips(gnrc_netif_t *outer, gnrc_netif_t *inner) {
-#if defined(BR_IPV6_ADDR) && defined(BR_IPV6_ADDR_LEN)
-    /* Add configured outer address */
-    ipv6_addr_t addr;
-    ipv6_addr_from_str(&addr, BR_IPV6_ADDR);
-    if (gnrc_netif_ipv6_addr_add(outer, &addr, BR_IPV6_ADDR_LEN, 0) < 0) {
-        printf("Failed setting outer address.\n");
-        return -1;
-    }
-#endif
-
-#ifdef BR_IPV6_DEF_RT
-    /* Add default route */
-    ipv6_addr_t defroute = IPV6_ADDR_UNSPECIFIED;
-    ipv6_addr_from_str(&addr, BR_IPV6_DEF_RT);
-    if (gnrc_ipv6_nib_ft_add(&defroute, 0, &addr, outer->pid, 0) < 0) {
-        printf("Failed setting default route.\n");
-        return -1;
-    }
-#endif
-
-    /* Turn off router advert on outer interface, it's really not our job. */
-    gnrc_ipv6_nib_change_rtr_adv_iface(outer, false);
-
-    /* Add inner address based on prefix and interface iid */
-    if (inner) {
-        eui64_t iid;
-        ipv6_addr_t prefix;
-        ipv6_addr_from_str(&prefix, BR_IPV6_PREFIX);
-        if (gnrc_netapi_get(inner->pid, NETOPT_IPV6_IID, 0, &iid, sizeof(iid)) < 0) {
-            printf("Failed getting wireless interface iid.\n");
-            return -1;
-        }
-        ipv6_addr_set_aiid(&prefix, iid.uint8);
-        if (gnrc_netif_ipv6_addr_add(inner, &prefix, 64, 0) < 0) {
-            printf("Failed setting outer address.\n");
-            return -1;
-        }
-    }
-
-#if GNRC_IPV6_NIB_CONF_MULTIHOP_P6C
-    /* Add as authoritative border router */
-    if (gnrc_ipv6_nib_abr_add(&prefix) < 0) {
-        printf("Failed adding prefix as authoritative border router.\n");
-        return -1;
-    }
-#endif
-
-#ifdef MODULE_GNRC_RPL
-    /* Configure rpl */
-    if (!inner || gnrc_rpl_init(inner->pid) < 0) {
-        printf("Failed initializing RPL on inner wireless interface.\n");
-        return -1;
-    }
-    gnrc_rpl_instance_t *inst = gnrc_rpl_instance_get(GNRC_RPL_DEFAULT_INSTANCE);
-    if (inst) {
-        gnrc_rpl_instance_remove(inst);
-    }
-    if (!gnrc_rpl_root_init(GNRC_RPL_DEFAULT_INSTANCE, &prefix, false, false)) {
-        printf("Failed initializing RPL root.\n");
-        return -1;
-    }
-#endif
-
-    return 0;
-}
-#endif//!!!!cleanup
 //--------@@
 
 /* @@
