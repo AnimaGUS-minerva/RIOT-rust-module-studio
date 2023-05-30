e85dad101a03a35642b8b9f85703fd23f7e0fd85
j-devel <38098238+j-devel@users.noreply.github.com>
2023-05-30 13:22:13 +0700

    Refactor `set_ips()`

--- examples/xbd-net/main/main.c
+++ examples/xbd-net/main/main.c
@@ -98,13 +98,13 @@ static void find_ifces(gnrc_netif_t **outer, gnrc_netif_t **inner) {
     print_ifce(*inner);
 }
 
-#if defined(MINERVA_BOARD_ESP32_ETH) || defined(MINERVA_BOARD_ESP32_WROOM32)
-static int set_ips(void) {
+#if defined(MINERVA_BOARD_ESP32_ETH) || defined(MINERVA_BOARD_ESP32_WROOM32)//!!!!cleanup
+static int set_ips(gnrc_netif_t *outer, gnrc_netif_t *inner) {
 #if defined(BR_IPV6_ADDR) && defined(BR_IPV6_ADDR_LEN)
     /* Add configured outer address */
     ipv6_addr_t addr;
     ipv6_addr_from_str(&addr, BR_IPV6_ADDR);
-    if (gnrc_netif_ipv6_addr_add(outer_interface, &addr, BR_IPV6_ADDR_LEN, 0) < 0) {
+    if (gnrc_netif_ipv6_addr_add(outer, &addr, BR_IPV6_ADDR_LEN, 0) < 0) {
         printf("Failed setting outer address.\n");
         return -1;
     }
@@ -114,29 +114,30 @@ static int set_ips(void) {
     /* Add default route */
     ipv6_addr_t defroute = IPV6_ADDR_UNSPECIFIED;
     ipv6_addr_from_str(&addr, BR_IPV6_DEF_RT);
-    if (gnrc_ipv6_nib_ft_add(&defroute, 0, &addr, outer_interface->pid, 0) < 0) {
+    if (gnrc_ipv6_nib_ft_add(&defroute, 0, &addr, outer->pid, 0) < 0) {
         printf("Failed setting default route.\n");
         return -1;
     }
 #endif
 
     /* Turn off router advert on outer interface, it's really not our job. */
-    gnrc_ipv6_nib_change_rtr_adv_iface(outer_interface, false);
+    gnrc_ipv6_nib_change_rtr_adv_iface(outer, false);
 
     /* Add inner address based on prefix and interface iid */
-    // @@ ignore `inner_interface`
-//    eui64_t iid;
-//    ipv6_addr_t prefix;
-//    ipv6_addr_from_str(&prefix, BR_IPV6_PREFIX);
-//    if (gnrc_netapi_get(inner_interface->pid, NETOPT_IPV6_IID, 0, &iid, sizeof(iid)) < 0) {
-//        printf("Failed getting wireless interface iid.\n");
-//        return -1;
-//    }
-//    ipv6_addr_set_aiid(&prefix, iid.uint8);
-//    if (gnrc_netif_ipv6_addr_add(inner_interface, &prefix, 64, 0) < 0) {
-//        printf("Failed setting outer address.\n");
-//        return -1;
-//    }
+    if (inner) {
+        eui64_t iid;
+        ipv6_addr_t prefix;
+        ipv6_addr_from_str(&prefix, BR_IPV6_PREFIX);
+        if (gnrc_netapi_get(inner->pid, NETOPT_IPV6_IID, 0, &iid, sizeof(iid)) < 0) {
+            printf("Failed getting wireless interface iid.\n");
+            return -1;
+        }
+        ipv6_addr_set_aiid(&prefix, iid.uint8);
+        if (gnrc_netif_ipv6_addr_add(inner, &prefix, 64, 0) < 0) {
+            printf("Failed setting outer address.\n");
+            return -1;
+        }
+    }
 
 #if GNRC_IPV6_NIB_CONF_MULTIHOP_P6C
     /* Add as authoritative border router */
@@ -148,7 +149,7 @@ static int set_ips(void) {
 
 #ifdef MODULE_GNRC_RPL
     /* Configure rpl */
-    if (gnrc_rpl_init(inner_interface->pid) < 0) {
+    if (!inner || gnrc_rpl_init(inner->pid) < 0) {
         printf("Failed initializing RPL on inner wireless interface.\n");
         return -1;
     }
@@ -164,7 +165,7 @@ static int set_ips(void) {
 
     return 0;
 }
-#endif// esp32 eth || wifi
+#endif//!!!!cleanup
 //--------@@
 
 /* @@
@@ -216,7 +217,7 @@ int main(void) {
 
     find_ifces(&outer_interface, &inner_interface);
 #if defined(MINERVA_BOARD_ESP32_ETH) || defined(MINERVA_BOARD_ESP32_WROOM32)
-    set_ips();
+    set_ips(outer_interface, inner_interface);
 #endif
 
     start_shell(shell_commands_minerva);
