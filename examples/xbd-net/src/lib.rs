a01885ce222cf5cd4e8d1aebcdc30af457bb5855
j-devel <38098238+j-devel@users.noreply.github.com>
2023-07-17 10:59:55 +0700

    spawn_local: reimplement with crossbeam_queue, support no_std, nested futures, cleanup

--- examples/xbd-net/src/lib.rs
+++ examples/xbd-net/src/lib.rs
@@ -1,16 +1,17 @@
 #![no_std]
-//#![feature(alloc_error_handler)]//@@ *** disabled, due to conflict with futures-channel
+#![feature(alloc_error_handler)]
 
-//@@ ***
-// #[panic_handler]
-// fn panic(info: &core::panic::PanicInfo) -> ! { mcu_if::panic(info) }
-//
-// #[alloc_error_handler]
-// fn alloc_error(layout: mcu_if::alloc::alloc::Layout) -> ! { mcu_if::alloc_error(layout) }
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! { mcu_if::panic(info) }
+
+#[alloc_error_handler]
+fn alloc_error(layout: mcu_if::alloc::alloc::Layout) -> ! { mcu_if::alloc_error(layout) }
 
 use core::cell::{Cell, RefCell};
 use core::future::Future;
 use mcu_if::{println, alloc::rc::Rc};
+use async_task::{Runnable, Task};
+use crossbeam_queue::ArrayQueue;
 
 #[no_mangle]
 pub extern fn rustmod_start() {
@@ -21,64 +22,51 @@ pub extern fn rustmod_start() {
 fn rustmod_tests() {
     println!("@@ rustmod_tests(): ^^");
 
-    //----@@ https://github.com/smol-rs/async-task/blob/9ff587ecab7b9a9fa81672f4dbf315ff375b6e5e/examples/spawn-local.rs#L51
+    //let queue = Rc::new(RefCell::new(futures_channel::mpsc::unbounded::<Runnable>()));
+    let queue = Rc::new(RefCell::new(crossbeam_queue::ArrayQueue::<Runnable>::new(99)));
+
+    //----@@ adaptation of https://github.com/smol-rs/async-task/blob/9ff587ecab7b9a9fa81672f4dbf315ff375b6e5e/examples/spawn-local.rs#L51
     let val = Rc::new(Cell::new(0));
+    println!("@@ rustmod_tests(): val: {}", val.get());
 
     // Run a future that increments a non-`Send` value.
-    run({
+    run(queue.clone(), {
         let val = val.clone();
         async move {
-            println!("@@ future1: ^^");
+            println!("@@ future1: ^^ val: {}", val.get());
+
             // Spawn a future that increments the value.
-            /*let task = spawn({
+            let task = spawn(queue, {
                 let val = val.clone();
                 async move {
-                    val.set(dbg!(val.get()) + 1);
+                    println!("@@ future2: ^^ val: {}", val.get());
+                    val.set(val.get() + 1);
+                    println!("@@ future2: $$ val: {}", val.get());
                 }
-            });*/
+            });
 
             val.set(val.get() + 1);
-            //task.await;
-            println!("@@ future1: $$");
+            task.await;
+
+            println!("@@ future1: $$ val: {}", val.get());
         }
     });
 
     // The value should be 2 at the end of the program.
-    val.get();
+    println!("@@ rustmod_tests(): $$ val: {}", val.get());
     //----@@
 }
 
-//
-
-use async_task::{Runnable, Task};
-use futures_channel::mpsc::{UnboundedSender,UnboundedReceiver};
-
-// FIXME !! for `ee`
-/*
-error[E0463]: can't find crate for `std`
-  |
-  = note: the `xtensa-esp32-none-elf` target may not support the standard library
-  = note: `std` is required by `futures_core` because it does not declare `#![no_std]`
-  = help: consider building the standard library from source with `cargo build -Zbuild-std`
-
-For more information about this error, try `rustc --explain E0463`.
-error: could not compile `futures-core` due to previous error
- */
-
 /// Spawns a future on the executor.
-//@@fn spawn<F, T>(future: F) -> Task<T>
-fn spawn<F, T>(queue: Rc<RefCell<(UnboundedSender<Runnable>, UnboundedReceiver<Runnable>)>>, future: F) -> Task<T>
+fn spawn<F, T>(queue: Rc<RefCell<ArrayQueue<Runnable>>>, future: F) -> Task<T>
 where
     F: Future<Output = T> + 'static,
     T: 'static,
 {
     println!("@@ spawn(): ^^");
     // Create a task that is scheduled by pushing itself into the queue.
-    //let schedule = |runnable| QUEUE.with(|(s, _)| s.send(runnable).unwrap());
-    //let (runnable, task) = async_task::spawn_local(future, schedule);
-    //==== @@
-    let schedule = |runnable| queue.borrow().0.unbounded_send(runnable).unwrap();
-    let (runnable, task) = unsafe { async_task::spawn_unchecked(future, schedule) };//@@ for no_std
+    let schedule = |runnable| queue.borrow().push(runnable).unwrap();
+    let (runnable, task) = unsafe { async_task::spawn_unchecked(future, schedule) };
 
     // Schedule the task by pushing it into the queue.
     runnable.schedule();
@@ -87,38 +75,33 @@ where
 }
 
 /// Runs a future to completion.
-fn run<F, T>(future: F) -> T
+fn run<F, T>(queue: Rc<RefCell<ArrayQueue<Runnable>>>, future: F) -> T
 where
     F: Future<Output = T> + 'static,
     T: 'static,
 {
-    let queue = Rc::new(RefCell::new(futures_channel::mpsc::unbounded::<Runnable>()));
+    println!("@@ run(): ^^");
 
     // Spawn a task that sends its result through a channel.
-    //let (s, r) = flume::unbounded();
-    //spawn(async move { drop(s.send(future.await)) }).detach();
-    //====@@
-    let (s, mut r) = futures_channel::mpsc::unbounded();
+    let oneshot = Rc::new(RefCell::new(crossbeam_queue::ArrayQueue::new(1)));
+    let oneshot_cloned = oneshot.clone();
     spawn(queue.clone(), async move {
-        println!("@@ future2: ^^");
-
-        drop(s.unbounded_send(future.await))
+        println!("@@ future-run: ^^");
+        drop(oneshot.borrow().push(future.await))
     }).detach();
 
     loop {
         println!("@@ loop: ^^");
         // If the original task has completed, return its result.
         //@@if let Ok(val) = r.try_recv() {
-        if let Ok(val) = r.try_next() {
-            //@@return val;
-            return val.unwrap();
+        if let Some(val) = oneshot_cloned.borrow().pop() {
+            return val;
         }
+        println!("@@ loop: --");
 
         // Otherwise, take a task from the queue and run it. (@@ invokes "future2" above)
-        //QUEUE.with(|(_, r)| r.recv().unwrap().run());
-        //====@@
-        println!("@@ loop: --");
-        queue.borrow_mut().1.try_next().unwrap().unwrap().run();
+        //@@QUEUE.with(|(_, r)| r.recv().unwrap().run());
+        queue.borrow().pop().unwrap().run();
         println!("@@ loop: $$");
     }
 }
