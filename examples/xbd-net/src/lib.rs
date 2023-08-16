bc2470739ee08d1e1a7973788a10c0aa6c27a562
j-devel <38098238+j-devel@users.noreply.github.com>
2023-08-16 11:25:58 +0700

    Add 'xbd.rs', refactor

--- examples/xbd-net/src/lib.rs
+++ examples/xbd-net/src/lib.rs
@@ -9,55 +9,12 @@ fn panic(info: &core::panic::PanicInfo) -> ! { mcu_if::panic(info) }
 fn alloc_error(layout: mcu_if::alloc::alloc::Layout) -> ! { mcu_if::alloc_error(layout) }
 
 use core::cell::Cell;
-use mcu_if::{println, alloc::{rc::Rc, boxed::Box}, c_types::c_void};
+use mcu_if::{println, alloc::{rc::Rc, boxed::Box}};
+use xbd::{Xbd, SleepFnPtr, SetTimeoutFnPtr};
 
+mod xbd;
 mod runtime;
 mod blogos12;
-
-// ---- xbd.rs !!
-
-type SleepFnPtr = unsafe extern "C" fn(u32);
-type SetTimeoutFnPtr = unsafe extern "C" fn(u32, *const c_void, *const c_void);
-pub struct Xbd {
-    _usleep: SleepFnPtr,
-    _ztimer_msleep: SleepFnPtr,
-    _ztimer_set: SetTimeoutFnPtr,
-}
-
-impl Xbd {
-    pub fn new(
-        xbd_usleep: SleepFnPtr,
-        xbd_ztimer_msleep: SleepFnPtr,
-        xbd_ztimer_set: SetTimeoutFnPtr
-    ) -> Self {
-        Self {
-            _usleep: xbd_usleep,
-            _ztimer_msleep: xbd_ztimer_msleep,
-            _ztimer_set: xbd_ztimer_set,
-        }
-    }
-
-    pub fn usleep(&self, usec: u32) {
-        unsafe { (self._usleep)(usec); }
-    }
-
-    pub fn msleep(&self, msec: u32) {
-        unsafe { (self._ztimer_msleep)(msec); }
-    }
-
-    pub fn set_timeout<F>(&self, msec: u32, cb: F) where F: FnOnce() + 'static {
-        let cb: Box<Box<dyn FnOnce()>> = Box::new(Box::new(cb));
-        let cb_ptr = Box::into_raw(cb) as *const _;
-
-        unsafe { (self._ztimer_set)(msec, Self::cb_handler as *const c_void, cb_ptr); }
-    }
-
-    fn cb_handler(cb_ptr: *const c_void) {
-        let cb: Box<Box<dyn FnOnce()>> = unsafe { Box::from_raw(cb_ptr as *mut _) };
-
-        (*cb)(); // call, move, drop
-    }
-}
 
 //
 
