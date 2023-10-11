2c8d055b95bc824437e9466f07bd51e5dbbdd752
j-devel <38098238+j-devel@users.noreply.github.com>
2023-10-11 12:27:24 +0700

    Disable blogos12 debug stuff

--- examples/xbd-net/src/runtime.rs
+++ examples/xbd-net/src/runtime.rs
@@ -1,8 +1,8 @@
 use core::future::Future;
 use super::xbd::process_xbd_callbacks;
 use super::blogos12::{
-    example_task as blogos12_example_task,
-    keyboard::print_keypresses as process_blogos12_scancodes,
+    //example_task as blogos12_example_task,
+    //keyboard::print_keypresses as process_blogos12_scancodes,
     executor::Executor,
 };
 
@@ -11,8 +11,8 @@ pub struct Runtime(Executor);
 impl Runtime {
     pub fn new() -> Result<Self, ()> {
         let mut ex = Executor::new();
-        ex.spawn(blogos12_example_task()) // debug
-            .spawn(process_blogos12_scancodes()) // processor, debug
+        ex//.spawn(blogos12_example_task()) // debug
+            //.spawn(process_blogos12_scancodes()) // processor, debug
             .spawn(process_xbd_callbacks()); // processor
 
         Ok(Self(ex))
