f643c38dfa0cfcfd86822f917d86b9724bf4de72
j-devel <38098238+j-devel@users.noreply.github.com>
2024-04-25 15:11:35 +0700

    rustmod.h: Clean up

--- examples/xbd-net/include/rustmod.h
+++ examples/xbd-net/include/rustmod.h
@@ -5,8 +5,3 @@ typedef struct xbd_fn_t {
 } xbd_fn_t;
 
 void rustmod_start(const xbd_fn_t *, size_t);
-bool get_kludge_force_no_async(void); // !!
-
-ssize_t xbd_riot_board_handler(coap_pkt_t *pdu, uint8_t *buf, size_t len, coap_request_ctx_t *ctx);
-
-void xbd_on_sock_udp_evt(sock_udp_t *sock, sock_async_flags_t type, void *arg);
