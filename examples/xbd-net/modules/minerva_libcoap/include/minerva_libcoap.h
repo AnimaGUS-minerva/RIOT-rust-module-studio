d12a94d0020d8fc57c179da97b60e05a7e83d3cc
j-devel <38098238+j-devel@users.noreply.github.com>
2024-04-25 15:16:29 +0700

    minerva_libcoap.h: Clean up

--- examples/xbd-net/modules/minerva_libcoap/include/minerva_libcoap.h
+++ examples/xbd-net/modules/minerva_libcoap/include/minerva_libcoap.h
@@ -5,16 +5,6 @@
 #ifndef MINERVA_LIBCOAP_H
 #define MINERVA_LIBCOAP_H
 
-//#include <stdint.h>
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//
-//#include "fmt.h"
-//#include "net/gcoap.h"
-//#include "net/utils.h"
-//#include "od.h"
-
 #ifdef __cplusplus
 extern "C" {
 #endif
