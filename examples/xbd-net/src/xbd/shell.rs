3f67eefaf6191d7127b149416f861049fb8eada6
j-devel <38098238+j-devel@users.noreply.github.com>
2024-05-02 17:12:29 +0700

    Add non-native board workaround to `process_shell_stream()`

--- examples/xbd-net/src/xbd/shell.rs
+++ examples/xbd-net/src/xbd/shell.rs
@@ -12,12 +12,17 @@ pub extern fn xbd_shell_on_read_line(/* TODO */) {
 static SD: StreamData<u8> = stream_uninit();
 
 pub async fn process_shell_stream() -> Result<(), i8> {
-    // TODO conditional !! native only for now
     let ret = unsafe { xbd_shell_init() };
-    if ret != 0 { return Err(ret); }
+    match ret {
+        0 => (), // ok, continue
+        2 => { // kludge
+            crate::println!("@@ process_shell_stream(): TODO - support non-native board");
+            return Ok(());
+        },
+        _ => return Err(ret),
+    }
 
     let mut stream = XbdStream::new(&SD);
-
     while let Some(cb) = stream.next().await {
         if 0 == 1 { crate::Xbd::async_sleep(1_000).await; } // debug, ok
 
