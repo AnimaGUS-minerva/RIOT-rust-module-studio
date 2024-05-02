c577d3af155129db2471501c80d784046aa5b31e
j-devel <38098238+j-devel@users.noreply.github.com>
2024-05-02 13:36:06 +0700

    shell.rs: Refactor with `XbdStream::from()`

--- examples/xbd-net/src/xbd/shell.rs
+++ examples/xbd-net/src/xbd/shell.rs
@@ -1,7 +1,5 @@
-use conquer_once::spin::OnceCell;
-use crossbeam_queue::ArrayQueue;
-use futures_util::{stream::StreamExt, task::AtomicWaker};
-use super::stream::XbdStream;
+use futures_util::stream::StreamExt;
+use super::stream::{XbdStream, StreamData, stream_uninit};
 
 extern "C" {
     fn xbd_shell_init() -> i8;
@@ -12,15 +10,14 @@ pub extern fn xbd_shell_on_read_line(/* TODO */) {
     crate::println!("@@ xbd_shell_on_read_line(): ^^");
 }
 
-static SERVER_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
-static SERVER_WAKER: AtomicWaker = AtomicWaker::new();
+static SD: StreamData<u8> = stream_uninit();
 
 pub async fn process_shell_stream() -> Result<(), i8> {
     // TODO conditional !! native only for now
     let ret = unsafe { xbd_shell_init() };
     if ret != 0 { return Err(ret); }
 
-    let mut stream = XbdStream::new(&SERVER_QUEUE, &SERVER_WAKER);
+    let mut stream = XbdStream::from(&SD);
 
     while let Some(cb) = stream.next().await {
         if 0 == 1 { crate::Xbd::async_sleep(1_000).await; } // debug, ok
