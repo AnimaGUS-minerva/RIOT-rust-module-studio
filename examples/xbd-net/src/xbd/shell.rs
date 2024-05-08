d25d65add74f1cee69769270f52681b72b5e5a20
j-devel <38098238+j-devel@users.noreply.github.com>
2024-05-08 15:53:19 +0700

    shell.rs: Refine code

--- examples/xbd-net/src/xbd/shell.rs
+++ examples/xbd-net/src/xbd/shell.rs
@@ -1,4 +1,5 @@
 use super::stream::{XbdStream, StreamData, stream_uninit, StreamExt};
+use crate::println;
 
 extern "C" {
     fn xbd_shell_init() -> i8;
@@ -11,31 +12,30 @@ static mut SHELL_BUF: heapless::String<SHELL_BUFSIZE> = heapless::String::new();
 
 #[no_mangle]
 pub extern fn xbd_shell_on_char(ch: u8) {
-    //crate::println!("@@ xbd_shell_on_char(): {}", ch);
+    //println!("@@ xbd_shell_on_char(): {}", ch);
 
     if let Some(xs) = prompt_is_ready() {
-        match ch {
-            10 => (), // ignore '\n'
-            0 => { // process '\0' (end of input)
+        match ch as char {
+            '\0' => { // end of input
                 unsafe {
                     if SHELL_BUF.len() < SHELL_BUFSIZE { // allow up to SHELL_BUFSIZE - 1
                         xs.add(SHELL_BUF.clone()); // send input
                         SHELL_BUF.clear();
                     } else {
-                        crate::println!("@@ input too long (> {}); to be ignored", SHELL_BUFSIZE - 1);
+                        println!("@@ input too long (> {}); to be ignored", SHELL_BUFSIZE - 1);
                         SHELL_BUF.clear();
                         xs.add(SHELL_BUF.clone()); // send empty input
                     }
                 }
             },
-            _ => {
-                let on_input_invalid = |_| {
-                    //crate::println!("@@ NOP; input (> SHELL_BUFSIZE={})", SHELL_BUFSIZE);
-                };
+            '\r' | '\n' => (), // ignore it
+            _ => { // keep it
                 unsafe {
                     SHELL_BUF
                         .push(ch as char)
-                        .unwrap_or_else(on_input_invalid);
+                        .unwrap_or_else(|_| { // input too long
+                            //println!("@@ NOP; input (> SHELL_BUFSIZE={})", SHELL_BUFSIZE);
+                        });
                 }
             },
         }
@@ -46,14 +46,13 @@ type StreamItem = heapless::String<{ SHELL_BUFSIZE }>;
 static SD: StreamData<StreamItem> = stream_uninit();
 
 pub async fn process_shell_stream() -> Result<(), i8> {
-    let bufsize = unsafe { xbd_shell_bufsize() };
-    assert_eq!(bufsize, SHELL_BUFSIZE);
+    assert_eq!(unsafe { xbd_shell_bufsize() }, SHELL_BUFSIZE);
 
     let ret = unsafe { xbd_shell_init() };
     match ret {
         0 => (), // ok, continue
         2 => { // kludge
-            crate::println!("@@ process_shell_stream(): TODO - support non-native board");
+            println!("@@ process_shell_stream(): TODO - support non-native board");
             return Ok(());
         },
         _ => return Err(ret),
@@ -63,12 +62,16 @@ pub async fn process_shell_stream() -> Result<(), i8> {
     prompt();
 
     while let Some(input) = stream.next().await {
-        if 0 == 1 { crate::Xbd::async_sleep(1_000).await; } // debug, ok
+        assert!(input.len() < SHELL_BUFSIZE);
+        println!("@@ [async shell] input: {} (len: {} SHELL_BUFSIZE: {})",
+                 input, input.len(), SHELL_BUFSIZE);
+        println!("@@ [async shell] input.as_bytes(): {:?}", input.as_bytes());
 
-        crate::println!("@@ [async shell] input: {} (len: {} bufsize: {})", input, input.len(), bufsize);
         match input {
             _ => (),
         }
+
+        if 0 == 1 { crate::Xbd::async_sleep(1_000).await; } // debug, ok
 
         prompt();
     }
